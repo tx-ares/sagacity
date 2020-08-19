ffc32911e8b869c252024ca1249ae346d330248f
tx-ares <twinadanz@gmail.com>
2020-08-18 20:36:41 -0500

    init commit

---
+++ js/sagacity.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function ($) {
+	jQuery(".sagacity-post-with-quote .x-blockquote").prepend("<i class='x-icon x-icon-chevron-up' data-x-icon='' aria-hidden='true'></i>");
+});
